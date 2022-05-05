bd2dcaf60577d864603b55a7ba96b74c476d9094
Komal <divyavairagade@gmail.com>
2022-05-06 01:33:00 +0530

    to find the max float value

--- RFP_FinMaxUsingGeneric/Program.cs
+++ RFP_FinMaxUsingGeneric/Program.cs
@@ -4,5 +4,13 @@
 using RFP_FindMaxUsingGeneric;
 
 Console.WriteLine("Find The Maximum Program");
+
+Console.WriteLine("--------Maximum integer value-----------");
 int output = MaxCheck.MaximumIntegerNumber(70, 100, 29);
 Console.WriteLine(output);
+Console.WriteLine();
+
+Console.WriteLine("--------Maximum float value-------------");
+double doubleoutput = MaxCheck.MaximumFloatNumber(22.11, 7.01, 29.02);
+Console.WriteLine(doubleoutput);
+Console.WriteLine();
