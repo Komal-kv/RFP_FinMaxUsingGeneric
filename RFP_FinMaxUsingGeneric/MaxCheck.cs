d8176054198aece0cdbd5695b018c68f97b8b15d
Komal <divyavairagade@gmail.com>
2022-05-06 01:27:32 +0530

    To find the max integer

--- RFP_FinMaxUsingGeneric/MaxCheck.cs
+++ RFP_FinMaxUsingGeneric/MaxCheck.cs
@@ -8,7 +8,22 @@ namespace RFP_FindMaxUsingGeneric
 {
     internal class MaxCheck
     {
-
+        public static int MaximumIntegerNumber(int First, int Second, int Third)
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                return First;
+            }
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                return Second;
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+                return Third;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
 
