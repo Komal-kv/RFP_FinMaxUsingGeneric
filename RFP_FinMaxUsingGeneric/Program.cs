ff465f3818a54fc32b5114f8abfa4b9825314383
Komal <divyavairagade@gmail.com>
2022-05-06 01:24:41 +0530

    Add project files.

---
+++ RFP_FinMaxUsingGeneric/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using RFP_FindMaxUsingGeneric;
+
+Console.WriteLine("Find The Maximum Program");
+int output = MaxCheck.MaximumIntegerNumber(70, 100, 29);
+Console.WriteLine(output);
